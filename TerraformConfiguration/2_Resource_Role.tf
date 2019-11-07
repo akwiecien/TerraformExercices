52818179567ba282d2a429fbe601a564d80ee74c
Andrzej Kwiecien <kwiecien_andrzej@interia.pl>
2019-11-07 09:06:42 +0100

    Attaching Policy to Rule

--- TerraformConfiguration/2_Resource_Role.tf
+++ TerraformConfiguration/2_Resource_Role.tf
@@ -6,3 +6,8 @@ resource "aws_iam_role" "LambdaCloudWachRole" {
     tag-key = "tag-vale"
   }
 }
+
+resource "aws_iam_role_policy_attachment" "AWSLambdaBasicExecutionRole-attach" {
+  role       = "${aws_iam_role.LambdaCloudWachRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
